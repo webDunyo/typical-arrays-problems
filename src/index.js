d7c964e55a08c525ee456b808980ada3a7a8fd28
Temurjon Sharifov <webdunyo@gmail.com>
2020-03-01 13:21:35 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    	return Math.min(...array);
+    }
+  	
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    	return Math.max(...array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let sum = 0;
+	if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    	for(let i = 0; i < array.length; i++ ){
+    		sum += array[i];
+    	}
+    	return sum / array.length;
+    }
 }
